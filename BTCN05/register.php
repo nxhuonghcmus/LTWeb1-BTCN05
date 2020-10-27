d448651b8611b01ee76cd4113b07e9f81ec341d4
nxhuong <xuanhuong8102000@gmail.com>
2020-10-27 18:56:03 +0700

    Update register.php

--- BTCN05/register.php
+++ BTCN05/register.php
@@ -122,7 +122,7 @@
                         <div class="form-group">
                             <div class="input-group">
                                 <button class="btn btn-primary btn-lg btn-block w3-round-large" type="submit" name="dki" value="dki">
-                                    Create Accounts</button>
+                                    Create Account</button>
                             </div>
                         </div>
                     </form>
